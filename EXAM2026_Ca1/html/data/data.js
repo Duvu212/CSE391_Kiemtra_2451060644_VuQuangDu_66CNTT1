29cb84cef37766e60500da618d71724ba1173d6f
Vu Quang Du <vqd212@gmail.com>
2026-06-10 13:52:15 +0700

    completed data

---
+++ EXAM2026_Ca1/html/data/data.js
@@ -0,0 +1,37 @@
+const data = [
+    {
+        id:1,
+        name: "Điện thoại ip 15",
+        DanhMuc: "Điện thoại",
+        Gia: "24.990.000 đ",
+        TrangThai: "Còn hàng"
+    },
+    {
+        id:2,
+        name: "Máy tính bảng Sámung Galaxy tab S9",
+        DanhMuc: "Máy tính bảng",
+        Gia: "18.500.000 đ",
+        TrangThai: "Còn hàng"
+    },
+    {
+        id:3,
+        name: "Chuột Logitech MX Master 35",
+        DanhMuc: "Phụ kiện",
+        Gia: "2.490.000 đ",
+        TrangThai: "Hết hàng"
+    },
+    {
+        id:4,
+        name: "Máy tính xách tay Macbook Air M3",
+        DanhMuc: "Laptop",
+        Gia: "31.990.000 đ",
+        TrangThai: "Còn hàng"
+    },
+    {
+        id:5,
+        name: "Tai nghe Sorry WH-1000XMS",
+        DanhMuc: "Tai nghe",
+        Gia: "7.990.000 đ",
+        TrangThai: "Còn hàng"
+    }
+];
